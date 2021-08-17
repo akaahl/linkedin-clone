0c2c0a9e101af3c878bc1c2da7da04dd634d9fb6
a.k.a.a.h.l <a.k.afiq@live.com>
2021-08-17 20:22:52 +0800

    Added Sidebar component

---
+++ src/Sidebar.js
@@ -0,0 +1,35 @@
+import { Avatar } from "@material-ui/core";
+import React from "react";
+import "./Sidebar.css";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <img src="" alt="top background" />
+        <Avatar className="sidebar__avatar" />
+
+        <h2>A.K Afiq</h2>
+        <h4>a.k.afiq@live.com</h4>
+      </div>
+
+      <div className="sidebar__stats">
+        <div className="sidebar__stat">
+          <p>Who viewed you</p>
+          <p className="sidebar__statNumber">2,543</p>
+        </div>
+
+        <div className="sidebar__stat">
+          <p>Views on post</p>
+          <p className="sidebar__statNumber">1,567</p>
+        </div>
+      </div>
+
+      <div className="sidebar__bottom">
+        <p>Recent</p>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
