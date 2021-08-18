05b1857d165638b1d301ca134418bad3565257c4
a.k.a.a.h.l <a.k.afiq@live.com>
2021-08-18 17:50:44 +0800

    Updated a Sidebar component

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,10 +3,15 @@ import React from "react";
 import "./Sidebar.css";
 
 function Sidebar() {
+  const recentItem = ["reactjs", "programming", "webdev", "uxuidesign"];
+
   return (
     <div className="sidebar">
       <div className="sidebar__top">
-        <img src="" alt="top background" />
+        <img
+          src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=753&q=80"
+          alt="top background"
+        />
         <Avatar className="sidebar__avatar" />
 
         <h2>A.K Afiq</h2>
@@ -27,6 +32,13 @@ function Sidebar() {
 
       <div className="sidebar__bottom">
         <p>Recent</p>
+        {recentItem &&
+          recentItem.map((item) => (
+            <div className="sidebar__recentItem">
+              <span className="sidebar__hash">#</span>
+              <p>{item}</p>
+            </div>
+          ))}
       </div>
     </div>
   );
